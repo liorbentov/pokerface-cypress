30c3509db4cb1a7c0e2642577b71529fb6dd7ae0
Lior Shub <liorbentov@gmail.com>
2018-01-22 18:50:39 +0200

    Add first test

---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,7 @@
+describe('Login to Pokerface.io', () => {
+   it('should have pokerface.io text', () => {
+        cy.visit('http://pokerface.io');
+        cy.get("[class^=title]")
+            .should("have.text", "Pokerface.io");
+    });
+});
