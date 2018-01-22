4f859eccfec2c5a3c4fced4d63cd465b5c1b186c
Lior Shub <liorbentov@gmail.com>
2018-01-23 00:11:46 +0200

    Add more tests to the login

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,6 +1,22 @@
 describe('Login to Pokerface.io', () => {
-	it('should have pokerface.io text', () => {
+	it('Document title should Pokerface.ic', () => {
 		cy.visit('http://pokerface.io');
+		cy.title().should('eq', 'Pokerface.io');
+	});
+
+	it('should have pokerface.io text', () => {
 		cy.get('[class^=title]').should('have.text', 'Pokerface.io');
+	});
+
+	it('should contain firstname input', () => {
+		cy.get('input[name="firstName"]').should('have.value', '');
+	});
+
+	it('should contain lastname input', () => {
+		cy.get('input[name="lastName"]').should('have.value', '');
+	});
+
+	it('should contain email input', () => {
+		cy.get('input[name="email"]').should('have.value', '');
 	});
 });
